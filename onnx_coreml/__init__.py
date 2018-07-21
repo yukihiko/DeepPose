ac672f202cd11b49a8d08291cd783c615f7d3b61
yukihiko <yukihiko.a@gmail.com>
2018-07-21 17:29:57 +0900

    m

--- onnx_coreml/__init__.py
+++ onnx_coreml/__init__.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-print('Start')
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
 
 from onnx_coreml.converter import convert
 
